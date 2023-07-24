72c0f5cad967749fa46b01a64211a1518527bfad
Shreedhar <urawane03@gmail.com>
2023-07-24 21:03:22 +0530

    filters

--- client/src/Components/CategoryProducts/Sidebar.jsx
+++ client/src/Components/CategoryProducts/Sidebar.jsx
@@ -6,7 +6,7 @@ const Sidebar = (props) => {
   return (
     <div className='h-screen bg-[var(--primary)] text-gray-200 w-0 lg:w-[250px] shrink-0 fixed top-0 z-10'>
       <div className='mt-24 hidden lg:block max-w-[80%] mx-auto'>
-        <h1>Apply Filters</h1>
+        <h1>Filters</h1>
         <div className='flex flex-col items-start gap-6 mt-8'>
           <h2>Price</h2>
           <div className='flex flex-col gap-2 items-start'>
